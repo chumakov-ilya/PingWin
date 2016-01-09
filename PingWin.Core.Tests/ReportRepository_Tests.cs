153b0433547c64c2cca3f95f2cf20e9ecf07afd4
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-09 16:52:31 +0300

    include lost files

---
+++ PingWin.Core.Tests/ReportRepository_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using PingWin.Entities;
+
+namespace PingWin.Core.Tests
+{
+	public class ReportRepository_Tests
+	{
+		[Test]
+		public void GetHourlyReport_Test()
+		{
+			var repo = new ReportRepository();
+
+			var rows = repo.GetHourlyReport();
+
+			Assert.IsNotNull(rows);
+		} 
+	}
+}
---
+++ PingWin.Entities/Extentions/DateTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PingWin.Entities
+{
+	public static class DateTimeExtentions
+	{
+		public static DateTime TruncateToSeconds(this DateTime dateTime) => TruncateTo(dateTime, TimeSpan.FromSeconds(1));
+		public static DateTime TruncateToHours(this DateTime dateTime) => TruncateTo(dateTime, TimeSpan.FromHours(1));
+
+		public static DateTime TruncateTo(this DateTime dateTime, TimeSpan timeSpan)
+		{
+
+			if (timeSpan == TimeSpan.Zero) return dateTime; 
+
+			return dateTime.AddTicks(-(dateTime.Ticks % timeSpan.Ticks));
+		}
+	}
+}
