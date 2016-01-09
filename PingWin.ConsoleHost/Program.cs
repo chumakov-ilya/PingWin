ca1b3b00e409e4480677c19ebee035b7c4982596
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-09 23:48:22 +0300

    added console host

---
+++ PingWin.ConsoleHost/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingWin.Core;
+using PingWin.Core.Triggers;
+
+namespace PingWin.ConsoleHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RunReports();
+
+			RunJobs();
+		}
+
+		private static void RunJobs()
+		{
+			var root = JobRoot.Default;
+
+			root.AddJob("test-http-success", 
+				new HttpRequestRule("http://httpbin.org/post").SetMethod("POST").ExpectCode(200))
+				.AttachTrigger(new MailTrigger());
+
+			root.AddJob("test-http-failure", 
+				new HttpRequestRule("http://httpbin.org/post"))
+				.AttachTrigger(new MailTrigger());
+
+			root.AddJob("test-db-success", new DbConnectionRule("Server=.;Database=PostoryDb;Trusted_Connection=True;"));
+			root.AddJob("test-db-failure", new DbConnectionRule("Server=.;Database=PostoryDbX;Trusted_Connection=True;"));
+
+			root.RunAll();
+		}
+
+		private static void RunReports()
+		{
+			var root = ReportRoot.Default;
+
+			root.AddReport(new Report());
+
+			Task.Run(() => root.RunAll());
+		}
+	}
+}
--- PingWin.Core/Jobs/JobDefaultSettings.cs
+++ PingWin.Core/Jobs/JobDefaultSettings.cs
@@ -4,7 +4,7 @@ namespace PingWin.Core
 {
 	internal static class JobDefaultSettings
 	{
-		public static TimeSpan CheckInterval { get; } = TimeSpan.FromSeconds(5);
-		public static TimeSpan FailureSilenceInterval { get; } = TimeSpan.FromSeconds(300);
+		public static TimeSpan CheckInterval { get; } = TimeSpan.FromMinutes(1);
+		public static TimeSpan FailureSilenceInterval { get; } = TimeSpan.FromMinutes(300);
 	}
 }
