54ffb0fe2799f8fe8b8f8e3f8f1411c249a10946
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-10 00:00:22 +0300

    added sql project

---
+++ PingWin.Sql/dbo/Tables/Log.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Log] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Status]      INT            NOT NULL,
+    [ShortData]   NVARCHAR (256) NULL,
+    [FullData]    NVARCHAR (MAX) NULL,
+    [StackTrace]  NVARCHAR (MAX) NULL,
+    [JobRecordId] INT            NOT NULL,
+    [DateTime]    DATETIME       NOT NULL,
+    CONSTRAINT [PK_Log] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Log_JobRecord] FOREIGN KEY ([JobRecordId]) REFERENCES [dbo].[JobRecord] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Status_Job_Date]
+    ON [dbo].[Log]([Status] ASC, [JobRecordId] ASC, [DateTime] ASC);
+
