94325271fe3205c6a70684b0b9c5c11bf3d6e430
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-10 16:56:41 +0300

    AsyncContext for console host

--- PingWin.ConsoleHost/Program.cs
+++ PingWin.ConsoleHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nito.AsyncEx;
 using PingWin.Core;
 
 namespace PingWin.ConsoleHost
@@ -11,36 +12,44 @@ namespace PingWin.ConsoleHost
 	{
 		static void Main(string[] args)
 		{
-			RunReports();
+			Task.Run(() => RunReports());
 
-			RunJobs();
+			AsyncContext.Run(() => RunJobs());
 		}
 
-		private static void RunJobs()
+		private static async void RunJobs()
 		{
 			var root = JobRoot.Default;
 
 			root.AddJob("test-http-success", 
 				new HttpRequestRule("http://httpbin.org/post").SetMethod("POST").ExpectCode(200))
-				.AttachTrigger(new MailTrigger());
+				.AttachTrigger(new MailTrigger())
+					.SetCheckIntervalSeconds(1)
+					.SetFailureSilenceIntervalSeconds(5);
 
 			root.AddJob("test-http-failure", 
 				new HttpRequestRule("http://httpbin.org/post"))
-				.AttachTrigger(new MailTrigger());
+				.AttachTrigger(new MailTrigger())
+					.SetCheckIntervalSeconds(1)
+					.SetFailureSilenceIntervalSeconds(5);
 
-			root.AddJob("test-db-success", new DbConnectionRule("Server=.;Database=PostoryDb;Trusted_Connection=True;"));
-			root.AddJob("test-db-failure", new DbConnectionRule("Server=.;Database=PostoryDbX;Trusted_Connection=True;"));
+			root.AddJob("test-db-success", new DbConnectionRule("Server=.;Database=PostoryDb;Trusted_Connection=True;"))
+					.SetCheckIntervalSeconds(1)
+					.SetFailureSilenceIntervalSeconds(5);
+			root.AddJob("test-db-failure", new DbConnectionRule("Server=.;Database=PostoryDbX;Trusted_Connection=True;"))
+					.SetCheckIntervalSeconds(1)
+					.SetFailureSilenceIntervalSeconds(5);
 
-			//root.RunAll();
+			await root.RunAllAsync();
 		}
 
-		private static void RunReports()
+		private static async void RunReports()
 		{
 			var root = ReportRoot.Default;
 
 			root.AddReport(new Report());
 
-			Task.Run(() => root.RunAllAsync());
+			await root.RunAllAsync();
 		}
 	}
 }
---
+++ PingWin.Core.Tests/DbConnectionRule_Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace PingWin.Core.Tests
+{
+	public class DbConnectionRule_Tests
+	{
+		[Test]
+		public void Execute_Test()
+		{
+			var rule = new DbConnectionRule("Server=.;Database=PostoryDb;Trusted_Connection=True;");
+
+			while (true)
+			{
+				rule.ExecuteAsync().Wait(); 
+			}
+		}
+	}
+}
