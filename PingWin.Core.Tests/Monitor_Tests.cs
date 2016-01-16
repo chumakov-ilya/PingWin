c277ea36615489e774a25c0069502e13f70356e3
Chumakov Ilya <aidforsoft@mail.ru>
2016-01-16 23:03:03 +0300

    Monitor class

---
+++ PingWin.Core.Tests/Monitor_Tests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace PingWin.Core.Tests
+{
+	public class Monitor_Tests
+	{
+		[Test]
+		public void Run_WhenCalledWithRealRoots_WaitsAndNoError()
+		{
+			var jobRoot = GetJobRootNotEmpty();
+
+			ReportRoot reportRoot = GetReportRootNotEmpty();
+
+			Monitor.Run(jobRoot, reportRoot);
+
+		}
+
+		private static ReportRoot GetReportRootNotEmpty()
+		{
+			var root = ReportRoot.Default;
+
+			root.AddReport(new Report())
+				.Delayed(false);
+
+			return root;
+		}
+
+		private static JobRoot GetJobRootNotEmpty()
+		{
+			var root = JobRoot.Default;
+
+			root.AddJob("test-http-success", new HttpRequestRule("http://httpbin.org/post").SetMethod("POST").ExpectCode(200));
+
+			root.AddJob("test-http-failure", new HttpRequestRule("http://httpbin.org/post"));
+
+			root.AddJob("test-db-success", new DbConnectionRule("Server=.;Database=PostoryDb;Trusted_Connection=True;"));
+
+			root.AddJob("test-db-failure", new DbConnectionRule("Server=.;Database=PostoryDbX;Trusted_Connection=True;"));
+
+			foreach (Job job in root.GetJobs())
+			{
+				job
+					//.AttachTrigger(new MailTrigger())
+					.SetLogSuccess(true)
+					.SetDebugSettings()
+					;
+			}
+
+			return root;
+		}
+	}
+}
---
+++ PingWin.Core/Monitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace PingWin.Core
+{
+	public static class Monitor
+	{
+		public static void Run(JobRoot jobRoot, ReportRoot reportRoot = null)
+		{
+			if (jobRoot == null) throw new ArgumentException("jobRoot is required");
+
+			var jobs = Task.Run(async () => await jobRoot.RunAllAsync());
+
+			if (reportRoot != null)
+			{
+				AsyncContext.Run(async () => await reportRoot.RunAllAsync()); 
+			}
+			else
+			{
+				jobs.Wait();
+			}
+		}
+	}
+}
